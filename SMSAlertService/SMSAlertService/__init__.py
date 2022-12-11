b84440bd2a901e6472b3ec0d00531122a155d47c
nicholaskonar <nkonar@protonmail.com>
2022-12-10 22:04:28 -0500

    edit secret key

--- SMSAlertService/SMSAlertService/__init__.py
+++ SMSAlertService/SMSAlertService/__init__.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 import SMSAlertService.views
 
-SMSAlertService.secret_key = os.environ['SMS_ALERT_SERVICE_SECRET_KEY']
+app.secret_key = os.environ['SMS_ALERT_SERVICE_SECRET_KEY']
 
 if __name__ == "__init__":
     SMSAlertService.app.run()
