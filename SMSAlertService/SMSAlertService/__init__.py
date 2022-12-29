d5910cbac5f653dda170a9c8588b944aed640fe4
nicholaskonar <nkonar@protonmail.com>
2022-12-29 02:03:01 -0500

    experimental ui updates

--- SMSAlertService/SMSAlertService/__init__.py
+++ SMSAlertService/SMSAlertService/__init__.py
@@ -11,4 +11,5 @@ app.secret_key = os.environ['SMS_ALERT_SERVICE_SECRET_KEY']
 if __name__ == "__init__":
     SMSAlertService.app.run()
 
-# FLASK_APP=SMSAlertService/__init__.py --> in terminal
+# export FLASK_APP=SMSAlertService/__init__.py --> in terminal
+# and export FLASK_ENV=development
