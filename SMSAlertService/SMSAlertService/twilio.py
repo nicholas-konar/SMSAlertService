9c0817c72685dc8f059e5f88927c716ba5480f69
nicholaskonar <nkonar@protonmail.com>
2023-01-08 02:54:59 -0500

    limit 1 ph per acct, update user when low units

--- SMSAlertService/SMSAlertService/twilio.py
+++ SMSAlertService/SMSAlertService/twilio.py
@@ -1,5 +1,7 @@
 import os
+
 from twilio.rest import Client
+
 from SMSAlertService import app
 
 twilio_number = os.environ['TWILIO_NUMBER']
@@ -8,15 +10,15 @@ auth_token = os.environ['TWILIO_AUTH_TOKEN']
 messaging_service_sid = os.environ['TWILIO_MESSAGING_SERVICE_SID']
 
 
-def send_alert(username, destination, link, keywords):
+def send_alert(username, destination, link, keywords, units):
     client = Client(account_sid, auth_token)
-    body = build_alert_body(link, keywords)
+    body = build_alert_body(link, keywords, units)
     message = client.messages.create(
         body=body,
         messaging_service_sid=messaging_service_sid,
         to=destination
     )
-    app.logger.debug(f'Message sent to {username} at: {destination} with SID: {message.sid}')
+    app.logger.info(f'Message sent to {username} at: {destination} with SID: {message.sid}')
     return message
 
 
@@ -28,26 +30,34 @@ def send_otp(destination, otp):
         messaging_service_sid=messaging_service_sid,
         to=destination
     )
-    app.logger.debug(f'OTP sent to {destination} with SID: {message.sid}')
+    app.logger.info(f'OTP sent to {destination} with SID: {message.sid}')
     return message
 
 
 def warn(destination, units):
+    app.logger.debug(f'ABOUT TO SEND WARNING TO {destination}')
     client = Client(account_sid, auth_token)
-    body = f'Warning: You only have {units} alerts left. Reload at www.smsalertservice.com/profile'
+    body = f'Heads up: You have {units} alert(s) left. Reload at www.smsalertservice.com/profile'
     message = client.messages.create(
         body=body,
         messaging_service_sid=messaging_service_sid,
         to=destination
     )
-    app.logger.debug(f'OTP sent to {destination} with SID: {message.sid}')
+    app.logger.info(f'Low unit warning sent to {destination} with SID: {message.sid}')
     return message
 
 
-def build_alert_body(link, keywords):
+def build_alert_body(link, keywords, units):
     formatted_keywords = format_keywords(keywords)
     subreddit = os.environ['REDDIT_SUBREDDIT']
-    return f'www.smsalertservice.com\n\nA post on {subreddit} matched the following keywords: {formatted_keywords}\n{link}'
+    if units == 0:
+        return f'You\'re out of alerts! Reload at www.smsalertservice.com/profile' \
+               f'\n\nA post on {subreddit} matched the following keywords: {formatted_keywords}\n{link}'
+    elif units < 5:
+        return f'Heads up: You only have {units} alert(s) left! Reload at www.smsalertservice.com/profile\n\n' \
+               f'A post on {subreddit} matched the following keywords: {formatted_keywords}\n{link}'
+    else:
+        return f'www.smsalertservice.com\n\nA post on {subreddit} matched the following keywords: {formatted_keywords}\n{link}'
 
 
 def build_otp_body(otp):
