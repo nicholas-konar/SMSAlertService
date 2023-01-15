a439834a84de06f72f7cfc833b297900e6743316
nicholaskonar <nkonar@protonmail.com>
2023-01-15 01:58:08 -0500

    verbiage, pp options, handle plural keywords

--- SMSAlertService/SMSAlertService/notification.py
+++ SMSAlertService/SMSAlertService/notification.py
@@ -10,12 +10,15 @@ def distribute():
             for user in users:
                 matching_keywords = []
                 for keyword in user['Keywords']:
-                    if keyword.lower() in str(post.title).lower() or keyword in str(post.selftext).lower():
+                    if keyword.lower() in str(post.title).lower() \
+                            or keyword.lower() in str(post.selftext).lower() \
+                            or keyword.lower() + 's' in str(post.title).lower() \
+                            or keyword.lower() + 's' in str(post.selftext).lower():
                         app.logger.info(f'Keyword match detected for user {user["Username"]}: "{keyword}"')
                         matching_keywords.append(keyword + ', ')
                 if matching_keywords and int(user['Units']) > 0 and not mongo.blacklisted(user):
                     message = twilio.send_alert(user['Username'], user['PhoneNumber'], post.url,
-                                                matching_keywords, int(user['Units']-1))
+                                                matching_keywords, int(user['Units'] - 1))
                     mongo.update_user_msg_data(user['Username'], message)
                     messages_sent += 1
     response = {
@@ -30,4 +33,3 @@ def send_otp(ph):
     mongo.save_otp(ph, otp)
     twilio.send_otp(ph, otp)
     app.logger.debug(f'Sent OTP to {ph}')
-
