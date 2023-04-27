4a59fc169ebff84e20cdbbb53983afe500191677
nicholaskonar <nkonar@protonmail.com>
2023-04-26 22:04:29 -0400

    add JS delete all keywords button

--- SMSAlertService/SMSAlertService/static/js/script.js
+++ SMSAlertService/SMSAlertService/static/js/script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
   var table = document.createElement("table");
   table.setAttribute("id", "keywordTable");
 
-  for (var i = 0; i < keywords.length; i++) {
+  for (var i = keywords.length - 1; i >= 0; i--) {
     var keyword = keywords[i];
 
     var button = document.createElement("button");
@@ -15,15 +15,16 @@ document.addEventListener("DOMContentLoaded", function() {
 
     var icon = document.createElement("i");
     icon.classList.add("fa", "fa-trash");
-    button.appendChild(icon);
 
     var cell1 = document.createElement("td");
     var cell2 = document.createElement("td");
     cell1.innerText = keyword;
-    cell2.appendChild(button);
 
     var row = document.createElement("tr");
     row.setAttribute("id", `row-${keyword}`)
+
+    button.appendChild(icon);
+    cell2.appendChild(button);
     row.appendChild(cell1);
     row.appendChild(cell2);
     table.appendChild(row);
@@ -40,7 +41,6 @@ document.addEventListener("DOMContentLoaded", function() {
       data: JSON.stringify({DeleteKeyword: keywordToDelete}),
       success: function(response) {
         row.parentNode.removeChild(row);
-        console.log(response);
       }
     });
   };
@@ -53,6 +53,25 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   }
 
+  function deleteAllKeywords() {
+    $.ajax({
+        url: "/delete-all-keywords",
+        type: "POST",
+        contentType: "application/json",
+        data: JSON.stringify({DeleteAllKeywords: true}),
+        success: function(response) {
+            while (table.rows.length > 0) {
+                table.deleteRow(0);
+            }
+        }
+    })
+  }
+
+  var deleteAllKeywordsButton = document.getElementById("deleteAllKeywordsButton");
+  deleteAllKeywordsButton.addEventListener("click", function() {
+    deleteAllKeywords();
+  });
+
   function addKeyword() {
     const newKeyword = document.getElementById("newKeyword").value;
     fetch("/add-keyword", {
@@ -62,7 +81,6 @@ document.addEventListener("DOMContentLoaded", function() {
     })
     .then(response => response.json())
     .then(data => {
-        console.log(data)
         var newRow = table.insertRow(0);
         newRow.setAttribute("id", `row-${newKeyword}`)
         var newCell1 = newRow.insertCell(0);
@@ -80,15 +98,22 @@ document.addEventListener("DOMContentLoaded", function() {
         icon.classList.add("fa", "fa-trash");
         button.appendChild(icon);
 
-        newCell1.innerHTML = data.NewKeyword;
+        newCell1.innerHTML = newKeyword;
         newCell2.appendChild(button)
 
+        var inputField = document.getElementById("newKeyword");
+        inputField.value = "";
     })
     .catch(error => console.error(error));
     }
     const addButton = document.getElementById("addKeywordButton");
     addButton.addEventListener("click", addKeyword);
 
+    const addKeywordForm = document.getElementById("addKeywordForm");
+    addKeywordForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+        addKeyword();
+    });
 });
 
 
