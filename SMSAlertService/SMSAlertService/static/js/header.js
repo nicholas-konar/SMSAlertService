e9a1f4fe05e032aa2899d054129d091e28165cf0
nicholaskonar <nkonar@protonmail.com>
2023-05-15 19:22:13 -0400

    build mobile menu

--- SMSAlertService/SMSAlertService/static/js/header.js
+++ SMSAlertService/SMSAlertService/static/js/header.js
@@ -1,5 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+    var overlay = document.createElement("div");
+    overlay.classList.add("overlay");
+    overlay.addEventListener("click", closeMobileMenu);
+
+    const header = document.querySelector('header');
     const openMobileMenuIcon = document.getElementById('openMobileMenuIcon');
     const closeMobileMenuIcon = document.getElementById('closeMobileMenuIcon');
     const mobileMenu = document.getElementById('mobileMenu');
@@ -10,10 +15,23 @@ document.addEventListener("DOMContentLoaded", function() {
     function openMobileMenu() {
         console.log('open menu');
         mobileMenu.classList.add('open');
+        addOverlay();
     }
 
     function closeMobileMenu() {
         console.log('close menu');
         mobileMenu.classList.remove('open');
+        removeOverlay();
+    }
+
+    function addOverlay() {
+        document.body.appendChild(overlay);
+        // Fake header overlay because z-index doesn't work with the css position
+        header.style.backgroundColor = '#666666';
+    }
+
+    function removeOverlay() {
+        document.body.removeChild(overlay);
+        header.style.backgroundColor = 'white';
     }
 });
