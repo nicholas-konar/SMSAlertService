5486d55e6b5a7b4eee209af80bb94eecab6763f7
nicholaskonar <nkonar@protonmail.com>
2023-06-25 03:31:27 -0400

    paypal creds bug fix

--- SMSAlertService/SMSAlertService/paypal.py
+++ SMSAlertService/SMSAlertService/paypal.py
@@ -4,16 +4,13 @@ import os
 
 from SMSAlertService import app
 
-sandbox_client_id = os.environ['PAYPAL_SANDBOX_CLIENT_ID']
-sandbox_client_secret = os.environ['PAYPAL_SANDBOX_CLIENT_SECRET']
-
 client_id = os.environ['PAYPAL_CLIENT_ID']
 client_secret = os.environ['PAYPAL_CLIENT_SECRET']
 
 
 def get_access_token():
     app.logger.info('Fetching access token from paypal.')
-    auth_string = f'{sandbox_client_id}:{sandbox_client_secret}'
+    auth_string = f'{client_id}:{client_secret}'
     auth_string_encoded = base64.b64encode(auth_string.encode('utf-8')).decode('utf-8')
 
     headers = {
