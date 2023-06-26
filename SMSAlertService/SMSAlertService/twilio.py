961d93c34aee984f24320e365138b0133e395db4
nicholaskonar <nkonar@protonmail.com>
2023-06-26 12:18:57 -0400

    logging fix

--- SMSAlertService/SMSAlertService/twilio.py
+++ SMSAlertService/SMSAlertService/twilio.py
@@ -10,10 +10,8 @@ messaging_service_sid = os.environ['TWILIO_MESSAGING_SERVICE_SID']
 def send_message(body: str, ph=None, admin: bool = False):
     client = Client(account_sid, auth_token)
     destination = os.environ['ADMIN_NUMBER'] if admin else ph
-    message = client.messages.create(
+    return client.messages.create(
         body=body,
         messaging_service_sid=messaging_service_sid,
         to=destination
     )
-    app.logger.debug(f'Message sent to {destination} with SID: {message.sid}')
-    return message
