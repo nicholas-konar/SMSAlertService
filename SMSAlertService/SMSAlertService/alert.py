c48d2cfe74346662790056313c56121ce24cb39a
nicholaskonar <nkonar@protonmail.com>
2023-06-26 15:24:59 -0400

    fix censor

--- SMSAlertService/SMSAlertService/alert.py
+++ SMSAlertService/SMSAlertService/alert.py
@@ -6,7 +6,7 @@ class Alert:
     def __init__(self, user, post, keywords):
         self.owner = user
         self.post = post
-        self.subreddit = post.subreddit.display_name.replace('gun', '***')
+        self.subreddit = post.subreddit.display_name.replace('Gun', '***')
         self.url = f'redd.it/{post.id}'
         self.keywords = util.format_keywords(keywords)
         app.logger.info(f'Preparing alert for {user.username}: r/{self.subreddit} - {self.keywords}')
