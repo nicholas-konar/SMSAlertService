0c3b835d322c792ce02dd7aff947e84f4144b36e
Nicholas Konar <nkonar@protonmail.com>
2025-04-15 17:40:32 -0400

    Fix imports/format

--- services/alert_service.py
+++ services/alert_service.py
@@ -1,7 +1,11 @@
-from SMSAlertService import twilio, app
-from SMSAlertService.dao import DAO
-from SMSAlertService.resources.sms_templates import ORDER_CONFIRMATION_MSG, STANDARD_ALERT_MSG, OTP_MSG
-from SMSAlertService.services.auth_service import AuthService
+import twilio, app
+from dao import DAO
+from resources.sms_templates import (
+    ORDER_CONFIRMATION_MSG,
+    STANDARD_ALERT_MSG,
+    OTP_MSG,
+)
+from services.auth_service import AuthService
 
 
 class AlertService:
@@ -10,12 +14,12 @@ class AlertService:
     def send_alerts(alerts):
         for alert in alerts:
             body = STANDARD_ALERT_MSG.format(
-                subreddit=alert.subreddit,
-                keywords=alert.keywords,
-                url=alert.url
+                subreddit=alert.subreddit, keywords=alert.keywords, url=alert.url
             )
             twilio_object = twilio.send_message(body=body, ph=alert.owner.phonenumber)
-            app.logger.info(f'Sent alert to {alert.owner.username} with SID {twilio_object.sid}')
+            app.logger.info(
+                f"Sent alert to {alert.owner.username} with SID {twilio_object.sid}"
+            )
             DAO.create_alert_record(alert.owner.id, twilio_object)
 
     @staticmethod
@@ -23,14 +27,16 @@ class AlertService:
         otp = AuthService.generate_otp()
         body = OTP_MSG.format(otp=otp)
         twilio_object = twilio.send_message(body=body, ph=phonenumber)
-        app.logger.info(f'Sent OTP to {phonenumber} with SID {twilio_object.sid}')
+        app.logger.info(f"Sent OTP to {phonenumber} with SID {twilio_object.sid}")
         return AuthService.hash_data(otp)
 
     @staticmethod
     def send_order_confirmation(user, order_description):
         body = ORDER_CONFIRMATION_MSG.format(order_description=order_description)
         message = twilio.send_message(body=body, ph=user.phonenumber)
-        app.logger.info(f'Notified {user.username} that their order was filled. SID: {message.sid}')
+        app.logger.info(
+            f"Notified {user.username} that their order was filled. SID: {message.sid}"
+        )
 
     @staticmethod
     def send_admin(body):
@@ -38,7 +44,7 @@ class AlertService:
 
     @staticmethod
     def process_status_update(sid, status):
-        if status.upper() == 'DELIVERED':
+        if status.upper() == "DELIVERED":
             DAO.confirm_delivery(sid)
         else:
             DAO.update_alert_status(sid, status)
